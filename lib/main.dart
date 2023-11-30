07dce3da6e15a69932920a0db47a9d890d3e6fdd
rolfheinzrostock <rolfrostock@gmail.com>
2023-11-30 17:09:57 -0300

    Refatoração de código, criação da pasta assets, inserção das imagens locais (Image.asset) correção dos warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+
+import 'components/tasks.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,24 +34,12 @@ class _MyAppState extends State<MyApp> {
           duration: Duration(milliseconds: 500),
           child: ListView(
             children: [
-              Task(
-                  'Aprender Flutter no intervalo do curso!',
-                  'https://hotmart.s3.amazonaws.com/product_pictures/d945a760-0964-4880-9b75-97f6cafb04bd/ADFLogohotmart.png',
-                  2),
-              Task(
-                  'Andar de Bike',
-                  'https://static.tuasaude.com/media/article/uc/bf/andar-de-bicicleta-emagrece_32679_l.jpg',
-                  5),
-              Task(
-                  'Meditar',
-                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                  1),
-              Task(
-                  'Ler',
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvbM1r4P3AkveqpDisR7ISPCgo69YNQtILqw&usqp=CAU',
-                  3),
-              Task('Jogar',
-                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 4),
+              Task('Aprender Flutter no intervalo do curso!',
+                  'assets/images/flutter.png', 2),
+              Task('Andar de Bike', 'assets/images/bike.webp', 5),
+              Task('Meditar', 'assets/images/meditar.jpge', 1),
+              Task('Ler', 'assets/images/ler.jpg', 3),
+              Task('Jogar', 'assets/images/jogar.jpg', 4),
             ],
           ),
         ),
@@ -61,173 +50,6 @@ class _MyAppState extends State<MyApp> {
             });
           },
           child: Icon(Icons.remove_red_eye),
-        ),
-      ),
-    );
-  }
-}
-
-class Task extends StatefulWidget {
-  final String nome;
-  final String foto;
-  final int dificuldade;
-
-  const Task(this.nome, this.foto, this.dificuldade, {Key? key})
-      : super(key: key);
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: Colors.blue,
-              ),
-              height: 140,
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Colors.white,
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: Colors.black26,
-                        ),
-                        width: 80,
-                        height: 100,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.network(
-                            widget.foto,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 200,
-                            child: Text(
-                              widget.nome,
-                              style: TextStyle(
-                                fontSize: 24,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.star,
-                                  size: 15,
-                                  color: (widget.dificuldade >= 1)
-                                      ? Colors.blue
-                                      : Colors.blue[100]),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 2)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 3)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 4)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 5)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      Container(
-                        height: 52,
-                        width: 82,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              nivel++;
-                            });
-                            print(nivel);
-                          },
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Icon(Icons.arrow_drop_up),
-                              Text(
-                                'Lvl Up',
-                                style: TextStyle(fontSize: 12),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: (widget.dificuldade > 0)
-                              ? (nivel / widget.dificuldade) / 10
-                              : 1,
-                        ),
-                        width: 200,
-                      ),
-                      Text(
-                        'Nível: $nivel',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
         ),
       ),
     );
